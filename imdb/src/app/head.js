0c30ae6fb7ca12bb5af401db10d7450717c99155
callmejkode <callmejkode@gmail.com>
2023-02-09 13:10:20 +0530

    project created

---
+++ imdb/src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDb Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This Website is a clone of IMdb" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
