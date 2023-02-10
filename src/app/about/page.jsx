cdc7bbda9e61e49c58360c4d4d579006773fb2dc
callmejkode <callmejkode@gmail.com>
2023-02-10 16:42:31 +0530

    About page created

---
+++ src/app/about/page.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-medium text-amber-500 mt-6 mb-3">About</h1>
+      <p className="pb-3 text-justify">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit
+        venenatis nisl et ultrices. Vestibulum ante ipsum primis in faucibus
+        orci luctus et ultrices posuere cubilia curae; Proin quis orci
+        imperdiet, egestas felis non, pellentesque nibh. Vivamus blandit urna
+        nibh, vitae posuere eros volutpat non.<br/></p>
+         <p className="pb-3 text-justify">Cras et faucibus massa. Curabitur
+        vel dolor vitae neque convallis tristique ac eget felis. Nam at interdum
+        ligula. Ut dignissim urna vitae lorem commodo, nec venenatis enim
+        interdum. Donec dictum nibh ut fringilla viverra. Aliquam posuere sapien
+        sed lacus egestas efficitur. Quisque ac diam sit amet turpis egestas
+        porttitor eu a lorem. Ut sodales id arcu nec egestas. Nullam leo ex,
+        dictum sed maximus a, semper non ipsum. Phasellus ultricies orci vel
+        tincidunt scelerisque.<br/></p>
+        {""}
+       <p className="pb-3 text-justify"> Nullam non lacinia eros. Duis rhoncus sit amet ante vulputate auctor.
+        Pellentesque erat velit, dictum vitae pretium eget, laoreet non sapien.
+        Aliquam dignissim, odio a iaculis aliquam, nunc dolor facilisis ligula,
+        sed porta ex est et dui. Aenean ullamcorper odio id purus iaculis, eget
+        lacinia sem iaculis. Proin sed diam eleifend quam euismod maximus vel
+        vel libero. Duis arcu ligula, blandit at dignissim at, efficitur quis
+        leo.{" "}
+      </p><br/>
+      <p className="pb-3 text-justify">Cras et faucibus massa. Curabitur
+        vel dolor vitae neque convallis tristique ac eget felis. Nam at interdum
+        ligula. Ut dignissim urna vitae lorem commodo, nec venenatis enim
+        interdum. Donec dictum nibh ut fringilla viverra. Aliquam posuere sapien
+        sed lacus egestas efficitur. Quisque ac diam sit amet turpis egestas
+        porttitor eu a lorem. Ut sodales id arcu nec egestas. Nullam leo ex,
+        dictum sed maximus a, semper non ipsum. Phasellus ultricies orci vel
+        tincidunt scelerisque.<br/></p>
+        <p className="pb-3 text-justify">Cras et faucibus massa. Curabitur
+        vel dolor vitae neque convallis tristique ac eget felis. Nam at interdum
+        ligula. Ut dignissim urna vitae lorem commodo, nec venenatis enim
+        interdum. Donec dictum nibh ut fringilla viverra. Aliquam posuere sapien
+        sed lacus egestas efficitur. Quisque ac diam sit amet turpis egestas
+        porttitor eu a lorem. Ut sodales id arcu nec egestas. Nullam leo ex,
+        dictum sed maximus a, semper non ipsum. Phasellus ultricies orci vel
+        tincidunt scelerisque.<br/></p>
+    </div>
+  );
+}
